ec1099f73bf3bc8bf6dfa244d0b9e519c4b62f3b
santymrk2-victor <santymc@icloud.com>
2023-10-02 00:24:01 -0300

    New name for rules, the songs logic and the songs content and the messages part is fixed

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,52 +1,69 @@
 import imgClock from '../assets/clock_white.svg'
+import { Link } from 'react-router-dom'
+//import Trip from '../components/Trip.jsx'
 
 const Calendar = () => {
   return(
     <main className="grid grid-row justify-items-center lg:grid-cols-2 lg:self-center gap-10 m-5 text-white">
-      <div className="hover:scale-110 transition duration-300 ease-in-out bg-black bg-opacity-25 backdrop-blur-xl p-5 m-5 rounded-lg text-xl">
+      <div className="w-full sm:w-10/12 hover:scale-105 transition duration-300 ease-in-out bg-black bg-opacity-25 backdrop-blur-xl p-5 m-5 rounded-lg text-md sm:text-xl">
         <div className=' flex flex-row mb-4 items-center content-center justify-left m-0'>
           <img className=" w-12 mr-4" src={imgClock}></img>
           <h2 className=" text-3xl font-bold">Viernes</h2>
         </div>
-        <p>08:00hs - Salida</p>
-        <p>11:30hs - Llegada</p>
-        <p>14:30hs - Almuerzo</p>
-        <p>17:30hs - Libre</p>
-        <p>21:00hs - Reunión</p>
-        <p>22:00hs - Cena</p>
+        <p>07:00hs - Salida desde ICEP</p>
+        <p>12:00hs - Almuerzo en viaje</p>
+        <p>13:00hs - Llegada estimada al predio</p>
+        <Link> {/*className='hover:text-gray-200'*/}
+          <p>14:30hs - Excursión</p>
+        </Link>
+        <p>18:00hs - Deportes / Libre</p>
+        <p>Tiempo para bañarse</p>
+        <p>20:00hs - Encuentro</p>
+        <p>21:00hs - Cena</p>
       </div>
-      <div className="hover:scale-110 transition duration-300 ease-in-out bg-black bg-opacity-25 backdrop-blur-xl p-5 m-5 rounded-lg text-xl">
+      <div className="w-full sm:w-10/12 hover:scale-105 transition duration-300 ease-in-out bg-black bg-opacity-25 backdrop-blur-xl p-5 m-5 rounded-lg text-md sm:text-xl">
         <div className='flex flex-row mb-4 items-center content-center justify-left m-0'>
           <img className="w-12 mr-4" src={imgClock}></img>
           <h2 className=" text-3xl font-bold">Sábado</h2>
         </div>
-        <p>14:30hs - Comida</p>
-        <p>14:30hs - Comida</p>
-        <p>14:30hs - Comida</p>
-        <p>14:30hs - Comida</p>
-        <p>14:30hs - Comida</p>
+        <p>08:00hs - Desayuno</p>
+        <p>08:30hs - Devocional</p>
+        <Link>
+          <p>10:00hs - Excursión</p>
+        </Link>
+        <p>13:00hs - Almuerzo en el dique</p>
+        <p>18:00hs - Deportes / Libre</p>
+        <p>Tiempo para bañarse</p>
+        <p>20:00hs - Encuentro</p>
+        <p>21:00hs - Cena</p>
       </div>
-      <div className="hover:scale-110 transition duration-300 ease-in-out bg-black bg-opacity-25 backdrop-blur-xl p-5 m-5 rounded-lg text-xl">
+      <div className="w-full sm:w-10/12 hover:scale-105 transition duration-300 ease-in-out bg-black bg-opacity-25 backdrop-blur-xl p-5 m-5 rounded-lg text-md sm:text-xl">
         <div className='flex flex-row mb-4 items-center content-center justify-left m-0'>
           <img className="w-12 mr-4" src={imgClock}></img>
           <h2 className=" text-3xl font-bold">Domingo</h2>
         </div>
-        <p>14:30hs - Comida</p>
-        <p>14:30hs - Comida</p>
-        <p>14:30hs - Comida</p>
-        <p>14:30hs - Comida</p>
-        <p>14:30hs - Comida</p>
+        <p>08:00hs - Desayuno</p>
+        <p>08:30hs - Devocional</p>
+        <Link>
+          <p>10:00hs - Excursión</p>
+        </Link>
+        <p>14:00hs - Almuerzo en el predio</p>
+        <p>Deportes / Libre</p>
+        <Link>
+          <p>17:00hs - Excursión</p>
+        </Link>
+        <p>Bañarse</p>
+        <p>21:00hs - Cena</p>
+        <p>22:00hs - Encuentro & Fogón</p>
       </div>
-      <div className="hover:scale-110 transition duration-300 ease-in-out bg-black bg-opacity-25 backdrop-blur-xl p-5 m-5 rounded-lg text-xl">
+      <div className="w-full sm:w-10/12 hover:scale-105 transition duration-300 ease-in-out bg-black bg-opacity-25 backdrop-blur-xl p-5 m-5 rounded-lg text-md sm:text-xl">
         <div className='flex flex-row mb-4 items-center content-center justify-left m-0'>
           <img className="w-12 mr-4" src={imgClock}></img>
           <h2 className=" text-3xl font-bold">Lunes</h2>
         </div>
-        <p>14:30hs - Comida</p>
-        <p>14:30hs - Comida</p>
-        <p>14:30hs - Comida</p>
-        <p>14:30hs - Comida</p>
-        <p>14:30hs - Comida</p>
+        <p>08:00hs - Desayuno</p>
+        <p>08:30hs - Devocional conjunto</p>
+        <p>10:00hs - Partida</p>
       </div>
     </main>
   )
--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -1,88 +1,98 @@
 import { useEffect, useState } from 'react'
 import imgCup from '../assets/cup_white.svg'
 import imgCrossHair from '../assets/crosshair_white.svg'
-//import { getDaysForEvent } from '../services/days'
+import Button from '../components/Button.jsx'
+import { getDaysForEvent } from '../services/days'
 
 const Messages = () => {
   const [days, setDays] = useState(null)
   const [dev, setDev] = useState(Array(3).fill(false))
-  const handleClick1 = () => {
-    console.log(dev)
-    setDev( !dev[0])
-    console.log(dev)
+  const handleClick = (index) => {
+    if (dev[index]) {
+      const newDev = [...dev]
+      newDev[index] = !newDev[index]
+      setDev(newDev)
+    } else {
+    const newDev = Array(3).fill(false)
+      newDev[index] = !newDev[index]
+      setDev(newDev)
+    }
   }
-  //useEffect(() => {getDaysForEvent().then(setDays)}, [])
-  useEffect(() => {setDays(-3)}, [])
+  useEffect(() => {getDaysForEvent().then(setDays)}, [])
 
   return(
-    <main className="grid grid-row justify-items-center self-center lg:self-center gap-10 m-5 text-white">
-      <h1 className='self-center text-5xl'>Principios revalorizados</h1>
+    <main className="grid grid-row justify-items-center lg:self-center gap-2 m-5 text-white">
       {days>0 && <h1 className='text-5xl font-bold'>Muy pronto</h1>}
-      {days==0 && <h1 className='text-3xl font-bold'>Estas preparado?</h1>}
+      {days==0 && <h1 className='text-3xl sm:text-4xl font-bold'>¿Estas preparado?</h1>}
+      {days<0 && <div className='drop-shadow-lg'><h1 className='self-center text-3xl text-center sm:text-5xl'>Principios revalorizados</h1></div>}
       {days<=-1 &&
-        <div className="hover:scale-110 transition duration-300 ease-in-out bg-black bg-opacity-25 backdrop-blur-xl p-5 m-5 rounded-lg text-xl">
-          <div className='flex flex-row items-center content-center justify-left m-0' onClick={handleClick1}>
-            <img className="w-12 mr-4" src={imgCup}></img>
-            <h1 className="text-3xl font-bold pointer-events-none">Dia 1</h1>
+      <Button
+        updateBoard={handleClick}
+        title="Día 1"
+        index={0}
+        state={dev}
+        imgURL={imgCup}
+      >
+        {dev[0] &&
+          <div className='block w-11/12 self-center'>
+            <p className='text-2xl'>{"{ Mat 5.1-12 }"}</p>
+            <div className='flex flex-row'>
+              <img className='w-6 m-2 self-start' src={imgCrossHair}></img>
+              <p className='text-base self-center'>1. ¿Que cualidades son las que Jesús revaloriza?</p>
+            </div>
+            <div className='flex flex-row'>
+              <img className='w-6 m-2 self-start' src={imgCrossHair}></img>
+              <p className='text-base self-center'>2. Nuestro entorno, ¿como ve esas cualidades?</p>
+            </div>
+            <div className='flex flex-row'>
+              <img className='w-6 m-2 self-start' src={imgCrossHair}></img>
+              <p className='text-base self-center'>3. ¿Como afectan estas cualidades a mi vida?</p>
+            </div>
           </div>
-          {dev[0] &&
-            <div className=''>
-              <p>{"{ Mat 5.1-12 }"}</p>
+        }
+      </Button>
+      }
+      {days<=-2 &&
+        <Button
+          updateBoard={handleClick}
+          title="Día 2"
+          index={1}
+          state={dev}
+          imgURL={imgCup}
+        >
+          {dev[1] &&
+            <div className='block w-11/12 self-center'>
+              <p className='text-xl'>{"{ Mat 5.21-22; 27-28; 33-34 }"}</p>
               <div className='flex flex-row'>
-                <img className='w-6 m-2' src={imgCrossHair}></img>
-                <p className='text-xl self-center'>1. Que cualidades son las que Jesús revaloriza?</p>
+                <img className='w-6 m-2 self-start' src={imgCrossHair}></img>
+                <p className='text-base self-center'>1. Estas normas que Jesus establecio, ¿sirven para algo?</p>
               </div>
               <div className='flex flex-row'>
-                <img className='w-6 m-2' src={imgCrossHair}></img>
-                <p className='text-xl self-center'>2. Nuestro entorno, como ve esas cualidades?</p>
+                <img className='w-6 m-2 self-start' src={imgCrossHair}></img>
+                <p className='text-base self-center'>2. En este momento, ¿algo de esto me pasa a mi?</p>
               </div>
               <div className='flex flex-row'>
-                <img className='w-6 m-2' src={imgCrossHair}></img>
-                <p className='text-xl self-center'>3. Como afectan estas cualidades a mi vida?</p>
+                <img className='w-6 m-2 self-start' src={imgCrossHair}></img>
+                <p className='text-base self-center'>3. ¿Que herramientas nos da Jesus para combatir esta forma de pensar</p>
               </div>
             </div>
           }
-        </div>
+        </Button>
       }
-
-      {days<=-2 &&
-        <div className="hover:scale-110 transition duration-300 ease-in-out bg-black bg-opacity-25 backdrop-blur-xl p-5 m-5 rounded-lg text-xl">
-        <div className='flex flex-row items-center content-center justify-left m-0' onClick={handleClick1}>
-          <img className="w-12 mr-4" src={imgCup}></img>
-          <h1 className="text-3xl font-bold pointer-events-none">Dia 2</h1>
-        </div>
-        {dev[0] &&
-          <div className=''>
-            <p>{"{ Mat 5.21-22; 27-28; 33-34 }"}</p>
-            <div className='flex flex-row'>
-                <img className='w-6 m-2' src={imgCrossHair}></img>
-                <p className='text-xl self-center'>1. Estas normas que Jesus establecio, sirven para algo?</p>
-              </div>
-              <div className='flex flex-row'>
-                <img className='w-6 m-2' src={imgCrossHair}></img>
-                <p className='text-xl self-center'>2. En este momento, algo de esto me pasa a mi?</p>
-              </div>
-              <div className='flex flex-row'>
-                <img className='w-6 m-2' src={imgCrossHair}></img>
-                <p className='text-xl self-center'>3. Que herramientas nos da Jesus para combatir esta forma de pensar</p>
-              </div>
-          </div>
-        }
-      </div>
-      }
-
       {days<=-3 &&
-        <div className="hover:scale-110 transition duration-300 ease-in-out bg-black bg-opacity-25 backdrop-blur-xl p-5 m-5 rounded-lg text-xl">
-        <div className='flex flex-row items-center content-center justify-left m-0' onClick={handleClick1}>
-          <img className="w-12 mr-4" src={imgCup}></img>
-          <h1 className="text-3xl font-bold pointer-events-none">Dia 3</h1>
-        </div>
-        {dev[0] &&
-          <div className=''>
-            <p>{"{ Mat 5.38-48 }"}</p>
-          </div>
-        }
-      </div>
+        <Button
+          updateBoard={handleClick}
+          title="Día 3"
+          index={2}
+          state={dev}
+          imgURL={imgCup}
+        >
+          {dev[2] &&
+            <div className='block w-11/12 self-center'>
+              <p className='text-xl'>{"{ Mat 5.38-48 }"}</p>
+            </div>
+          }
+        </Button>
       }
     </main>
   )

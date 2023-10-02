9a900cc2c18adfbc3dc0f3f18e7173f7486439cb
santymrk2-victor <santymc@icloud.com>
2023-10-02 12:12:01 -0300

    Fixed the width of the button component in small displays

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ const Square = ({ children, updateBoard, index, title, imgURL }) => {
   }
 
   return (
-    <div className="w-10/12 hover:scale-100 transition duration-300 ease-in-out bg-black bg-opacity-25 backdrop-blur-xl p-5 m-5 rounded-lg text-sm sm:text-xl">
+    <div className="w-full sm:w-8/12 hover:scale-100 transition duration-300 ease-in-out bg-black bg-opacity-25 backdrop-blur-xl p-5 m-5 rounded-lg text-sm sm:text-xl">
       <div onClick={handleClick} className='flex flex-row items-center content-center justify-left m-0 transition-opacity	' >
         <img className=" w-12 mr-4 pointer-events-none" src={imgURL}></img>
         <h2 className="text-xl sm:text-2xl font-bold pointer-events-none">{title}</h2>
